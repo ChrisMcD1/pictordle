47aa853366e736c73653deb8d53ae87aeb1a02e7
Chris McDonnell <camcdonn@gmail.com>
2023-08-23 00:37:44 -0400

    First successful version to fully deploy and get a response!

--- app-server/main.go
+++ app-server/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"os"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -29,35 +22,6 @@ type Post struct {
 }
 
 func RootPath(w http.ResponseWriter, r *http.Request) {
-	sess := session.Must(session.NewSession())
-	svc := dynamodb.New(sess)
-
-	book := Post{
-		User:           "Test-123",
-		Day:            time.Now(),
-		Interpretation: "I see some cool clouds",
-	}
-
-	av, err := dynamodbattribute.MarshalMap(book)
-	if err != nil {
-		log.Fatalf("Got error marshalleling my new book %s", err)
-	}
-
-	tableName := os.Getenv("POST_TABLE_NAME")
-
-	input := &dynamodb.PutItemInput{
-		Item:      av,
-		TableName: aws.String(tableName),
-	}
-
-	_, err = svc.PutItem(input)
-	if err != nil {
-		log.Fatalf("Got error calling PutItem %s", err)
-	}
-
 	w.Header().Set("Content-Type", "text/plain")
-	_, err = fmt.Fprint(w, "Successfuly created item")
-	if err != nil {
-		log.Fatalf("Failed to write to response %s", err)
-	}
+	_, _ = fmt.Fprint(w, "Hello from my ECS instance!")
 }
